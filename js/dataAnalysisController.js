9754b93d91bdc60314c3d0b596e15e40ed73f625
GG2 <george7551858@gmail.com>
2016-01-27 01:48:53 +0800

    fix: user should not trigger "submit" if dygraph.js doesn't loaded.

--- js/dataAnalysisController.js
+++ js/dataAnalysisController.js
@@ -1,13 +1,15 @@
-var g;
+
 function init_dygraph() {
-    g = new Dygraph(
-    // containing div
-    document.getElementById("visualization"),
-    $("#data-text").val());
+    var g = new Dygraph(
+        // containing div
+        document.getElementById("visualization"),
+        $("#data-text").val()
+    );
+
+    $("#data-form").on('submit', function(event) {
+        event.preventDefault();
+        var data = $("#data-text").val();
+        g.updateOptions( { 'file': data } );
+    });
 }
 
-$("#data-form").on('submit', function(event) {
-    event.preventDefault();
-    var data = $("#data-text").val();
-    g.updateOptions( { 'file': data } );
-});
